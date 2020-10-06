9bc148348465004916def460af0d5826f4956c4d
591346 <69771520+591346@users.noreply.github.com>
2020-10-06 09:37:46 +0200

    test

--- src/no/hvl/dat100ptc/TODO.java
+++ src/no/hvl/dat100ptc/TODO.java
@@ -6,7 +6,7 @@ public class TODO {
 		
 		String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
 		
-		return "Metoden " + methodName + " er ikke implementert";
+		return "noe " + methodName + " er ikke implementert";
 	}
 	
 	
